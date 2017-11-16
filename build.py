1d28a4337d73bd7e1cf816a54c232dcd018b0567
David Lai <3357009+davidlatwe@users.noreply.github.com>
2017-11-17 04:03:18 +0800

    improve comment

--- build.py
+++ build.py
@@ -195,7 +195,8 @@ if __name__ == '__main__':
     resourcesdir = os.path.join(cd, "resources", kwargs.project)
     projectdir = os.path.join(cd, "projects", kwargs.project)
     fname = os.path.join(resourcesdir, ".recipe.json")
-    # (davidpower) Set this env-var up for later querying from database
+    # (davidlatwe) Set this environment variable for later
+    #              querying from database during import/export
     os.environ['AVALON_PROJECT'] = kwargs.project
 
     try:
